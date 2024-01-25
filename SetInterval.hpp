a361712a71f3358c8e424d371f1fc8547e91cfa8
kotinash <92477814+kotinash@users.noreply.github.com>
2024-01-26 00:18:09 +0100

    added settimeout

--- SetInterval.hpp
+++ SetInterval.hpp
@@ -30,3 +30,29 @@
             }                                                                     \
         }).detach();                                                              \
     } while (0)
+
+/**
+ * @def setTimeout(code, timeout)
+ * @brief Executes the specified code block after a specified timeout.
+ *
+ * This macro creates a C++ thread that waits for the specified timeout
+ * and then executes the provided code block.
+ *
+ * @param code The code block to be executed after the timeout.
+ * @param timeout The time duration (in milliseconds) to wait before executing the code block.
+ *
+ * Example usage:
+ * @code
+ * setTimeout({
+ *     std::cout << "Delayed execution!";
+ * }, 2000);
+ * @endcode
+ */
+#define setTimeout(code, timeout)                                            \
+    do {                                                                     \
+        std::thread([=]() {                                                  \
+            std::this_thread::sleep_for(std::chrono::milliseconds(timeout)); \
+                                                                             \
+            code;                                                            \
+        }).detach();                                                         \
+    } while (0)
